7c5db687cfb61b1834a16d10ca9b36ded51b1c8c
ghianluca <ribeiroghian@gmail.com>
2024-11-14 00:59:13 -0300

    add createnutritionservice and integrate with createnutritioncontroller

--- src/controllers/CreateNutritionController.ts
+++ src/controllers/CreateNutritionController.ts
@@ -1,10 +1,15 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { CreateNutritionService } from '../services/CreateNutritionService'
 
-class CreateNutritionController{
+class CreateNutritionController {
     async handle(request: FastifyRequest, reply: FastifyReply){
         console.log("Deu certo, rota chamada!!")
 
-        reply.send({ message: "Aí sim meu cumpadi, rota chamada!!" })
+        const createNutrition = new CreateNutritionService();
+
+        const nutrition = await createNutrition.execute();
+
+        reply.send(nutrition);
     }
 }
 
---
+++ src/services/CreateNutritionService.ts
@@ -0,0 +1,9 @@
+class CreateNutritionService {
+    async execute(){
+        console.log("Test")
+
+        return { message: "É isso ai, deu certo o service!" }
+    }
+}
+
+export { CreateNutritionService }

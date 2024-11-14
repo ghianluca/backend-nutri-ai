245337750733fdc13dfa4fe8704fb64e85f0a1a7
ghianluca <ribeiroghian@gmail.com>
2024-11-14 00:02:46 -0300

    add dotenv configuration and custom error handler

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,8 @@ import cors from '@fastify/cors';
 import dotenv from 'dotenv';
 
 const app = Fastify({ logger: true })
+dotenv.config();
+
+app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({ message: error.message })
+})
